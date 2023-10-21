9211bb118a99c726ae0ccefac4e6f35962ae72ef
Lukas Wijgaart van Dijk <79897805+lukaswvd@users.noreply.github.com>
2023-10-21 16:47:34 +0000

    Test

--- price-overview.py
+++ price-overview.py
@@ -10,7 +10,7 @@ except ImportError:
     exit("This script requires the requests module\nInstall with: sudo pip install requests")
 
 inky_display = auto()
-inky_display.set_border(inky_display.WHITE)
+inky_display.set_border(inky_display.BLACK)
 
 currentDateAndTime = datetime.now()
 price = ""
@@ -26,8 +26,6 @@ else:
 img = Image.new("P", (inky_display.WIDTH, inky_display.HEIGHT))
 draw = ImageDraw.Draw(img)
 
-
-
 font = ImageFont.truetype(FredokaOne, 22)
 
 w, h = font.getsize(price)
@@ -35,6 +33,5 @@ x = (inky_display.WIDTH / 2) - (w / 2)
 y = (inky_display.HEIGHT / 2) - (h / 2)
 
 draw.text((x, y), price, inky_display.RED, font)
-
-inky_display.set_image(Image.open("./resources/NO1.png"))
+inky_display.set_image(Image.open("/resources/NO1.png").convert('P'))
 inky_display.show()
