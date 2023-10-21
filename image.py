e417bd437ec08a59f320216f9eae10dbdeae7561
Lukas Wijgaart van Dijk <79897805+lukaswvd@users.noreply.github.com>
2023-10-21 17:09:18 +0000

    Fix

--- image.py
+++ image.py
@@ -1,9 +1,12 @@
+import os
 from inky import InkyPHAT
 from PIL import Image, ImageFont, ImageDraw
+
+PATH = os.path.dirname(__file__)
 
 inky_display = InkyPHAT("red")
 inky_display.set_border(inky_display.WHITE)
 
-img = Image.open("./resources/NO1.png")
+img = Image.open(os.path.join(PATH, "resources/NO1.png"))
 inky_display.set_image(img)
 inky_display.show()
