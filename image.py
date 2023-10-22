21ca06daad035dec1bfd75ff5e17a71e1578c36e
Lukas Wijgaart van Dijk <79897805+lukaswvd@users.noreply.github.com>
2023-10-22 09:52:16 +0000

    Text

--- image.py
+++ image.py
@@ -1,6 +1,7 @@
 import os
 from inky import InkyPHAT
 from PIL import Image, ImageFont, ImageDraw
+from font_fredoka_one import FredokaOne
 
 PATH = os.path.dirname(__file__)
 
@@ -8,5 +9,11 @@ inky_display = InkyPHAT("red")
 inky_display.set_border(inky_display.WHITE)
 
 img = Image.open(os.path.join(PATH, "resources/NO1.png"))
+draw = ImageDraw.Draw(img)
+
+font = ImageFont.truetype(FredokaOne, 22)
+
+draw.text((72, 34), "62 øre/kWh", inky_display.BLACK, font=font)
+
 inky_display.set_image(img)
 inky_display.show()
