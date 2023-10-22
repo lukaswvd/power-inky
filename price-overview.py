e8678eae9624eff36d6a98b1a937d1f388f214df
Lukas Wijgaart van Dijk <79897805+lukaswvd@users.noreply.github.com>
2023-10-22 10:16:21 +0000

    Clean up

--- price-overview.py
+++ price-overview.py
@@ -1,5 +1,6 @@
+import os
 import json
-from inky.auto import auto
+from inky import InkyPHAT
 from PIL import Image, ImageFont, ImageDraw
 from font_fredoka_one import FredokaOne
 from datetime import datetime
@@ -9,8 +10,13 @@ try:
 except ImportError:
     exit("This script requires the requests module\nInstall with: sudo pip install requests")
 
-inky_display = auto()
-inky_display.set_border(inky_display.BLACK)
+PATH = os.path.dirname(__file__)
+
+inky_display = InkyPHAT("red")
+inky_display.set_border(inky_display.WHITE)
+
+img = Image.open(os.path.join(PATH, "resources/NO1.png"))
+draw = ImageDraw.Draw(img)
 
 currentDateAndTime = datetime.now()
 price = ""
@@ -19,19 +25,18 @@ res = requests.get("https://www.hvakosterstrommen.no/api/v1/prices/{}/{}-{}_NO1.
 
 if res.status_code == 200:
     j = json.loads(res.text)
-    price = "{:.3f} øre/kWh".format(j[currentDateAndTime.hour]["NOK_per_kWh"])
+    # price in øre and with taxes
+    price = "{:.0f} øre/kWh".format(j[currentDateAndTime.hour]["NOK_per_kWh"]*125)
 else:
     price = "Kunne ikke hente pris"
 
-img = Image.new("P", (inky_display.WIDTH, inky_display.HEIGHT))
-draw = ImageDraw.Draw(img)
 
 font = ImageFont.truetype(FredokaOne, 22)
 
 w, h = font.getsize(price)
-x = (inky_display.WIDTH / 2) - (w / 2)
 y = (inky_display.HEIGHT / 2) - (h / 2)
 
-draw.text((x, y), price, inky_display.RED, font)
+draw.text((72, y), price, inky_display.BLACK, font=font)
+
 inky_display.set_image(img)
 inky_display.show()
